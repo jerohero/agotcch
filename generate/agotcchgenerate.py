1856363689783132074c37405b12c2562d44ae71
Jeroen <jegbol01@gmail.com>
2024-08-03 23:11:02 +0900

    Find father/mother info

--- generate/agotcchgenerate.py
+++ generate/agotcchgenerate.py
@@ -53,6 +53,8 @@ def get_birth_effect(character):
     def get_sexuality():
         if character["sexuality"]:
             return f'set_sexuality = {character["sexuality"]}\n'
+        
+        return ''
 
     def get_inherited_traits():
         output = ''
@@ -91,8 +93,7 @@ def get_birth_effect(character):
 
         return textwrap.indent(output, indent + indent + indent).rstrip()
 
-
-    bigass_string = textwrap.dedent(f"""
+    birth_effect = textwrap.dedent(f"""
         # {character["name"]["primary"]} - {character["id"]}
         agot_canon_children_{character["id"]}_birth_effect = {{
             scope:child = {{
@@ -106,7 +107,8 @@ def get_birth_effect(character):
             {inject_data()}
         }}
     """)
-    return bigass_string
+
+    return birth_effect
 
 def init_character():
     return {
@@ -118,6 +120,9 @@ def init_character():
         "birth": 0,
         "is_female": False,
         "house": "",
+        "father": "",
+        "real_father": "",
+        "mother": "",
         "dna": "",
         "sexuality": "",
         "employer:": "",
@@ -146,9 +151,14 @@ def init_character():
         ]
     }
 
+def get_nested_value(value):
+    return value[value.find('=') + 1 : value.find('}')].strip() # eg. x = { y = z }
+
 def process_lines():
     is_reading_character = False
     current_year_block = 0
+
+    characters = []
 
     for i, line in enumerate(lines):
         is_line_empty = not line or line[0] == '#'
@@ -164,9 +174,7 @@ def process_lines():
         if is_line_character_name:
             # Print previous character
             if is_reading_character:
-                print(get_birth_effect(character))
-                # print(json.dumps(character, sort_keys=True, indent=4))
-
+                characters.append(character)
             is_reading_character = False
 
             # character = Character()
@@ -215,6 +223,16 @@ def process_lines():
                 elif key == "female":
                     if value == "yes":
                         character["is_female"] = True
+                # Father
+                elif key == "father":
+                    character["father"] = value
+                # Real father
+                elif key == "effect" and "set_real_father" in value:
+                    real_father_id = get_nested_value(value)
+                    character["real_father"] = real_father_id
+                # Mother
+                elif key == "mother":
+                    character["mother"] = value
                 # DNA
                 elif key == "dna":
                     character["dna"] = value
@@ -248,15 +266,58 @@ def process_lines():
                     # TODO converting, use_liege_for_heir
                     is_first_guardian = character["guardian"]["primary"]["id"] == ""
                     if is_first_guardian:
-                        guardian_id = value[value.find('=') + 1 : value.find('}')].strip()
+                        guardian_id = get_nested_value(value)
                         character["guardian"]["primary"]["id"] = guardian_id
                 # Nickname
                 elif key == "give_nickname":
                     if is_birth_block:
                         character["nickname"] = value
-                # Scripted appearance flags + traits
-                # On birth
-                
+                # TODO Scripted appearance flags + traits
+                # TODO On birth
+
+    for character in characters:
+        continue
+        # print(get_birth_effect(character))
+        # print(json.dumps(character, sort_keys=True, indent=4))
+    
+    generate_family_trees(characters)
+
+def group_by_parent(children, parent_id_cb):
+    parent_dict = {}
+
+    for child in children:
+        child_id = child["id"]
+        parent_id = parent_id_cb(child)
+
+        if parent_id not in parent_dict:
+            parent_dict[parent_id] = []
+        parent_dict[parent_id].append(child_id)
+        
+    # result = [{'id': parent_id, 'children': children} for parent_id, children in parent_dict.items()]
+
+    return parent_dict
+
+# For making sure the child will be set up as a canon mother on birth
+def find_mother_chains(mothers):
+    mother_chains = []
+
+    for mother_id, child_ids in mothers.items():
+        for child_id in child_ids:
+            if child_id in mothers:
+                mother_chains.append(child_id)
+
+
+def generate_family_trees(characters):
+    def father_id_cb(character):
+        return character["real_father"] if character["real_father"] else character["father"]
+
+    def mother_id_cb(character):
+        return character["mother"]
+
+    fathers = group_by_parent(characters, father_id_cb)
+    mothers = group_by_parent(characters, mother_id_cb)
+    mother_chains = find_mother_chains(mothers)
+
                 
 
 folder_path = 'D:/projects/agotcch/generate/characters'
