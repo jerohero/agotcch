40941f758df80bcbb0e3b0351a06253ac5053927
Jeroen <jegbol01@gmail.com>
2024-09-27 20:51:45 +0200

    Space to tabs

--- generate/dummy_characters.py
+++ generate/dummy_characters.py
@@ -1,23 +1,23 @@
 import textwrap
 
 def generate_dummy_characters(characters, dnas):
-    dummy_characters = []
+	dummy_characters = []
 
-    for child_id, child in characters.items():
-        if child_id in dnas:
-            dummy_characters.append(create_dummy_character(child))
+	for child_id, child in characters.items():
+		if child_id in dnas:
+			dummy_characters.append(create_dummy_character(child))
 
-    return ''.join(dummy_characters).lstrip()
+	return ''.join(dummy_characters).lstrip()
 
 def create_dummy_character(child):
-    return textwrap.dedent(f"""
-        Dummy_{child["id"]} = {{
-            name = {child["name"]["primary"]}
-            dna = {child["id"]}
-            religion = the_seven_main
-            culture = northman
-            father = Ruin_Empress
-            118.1.1 = {{ birth = yes }}
-            118.1.2 = {{ death = yes }}
-        }}
-    """)
+	return textwrap.dedent(f"""
+		Dummy_{child["id"]} = {{
+			name = {child["name"]["primary"]}
+			dna = {child["id"]}
+			religion = the_seven_main
+			culture = northman
+			father = Ruin_Empress
+			118.1.1 = {{ birth = yes }}
+			118.1.2 = {{ death = yes }}
+		}}
+	""")
